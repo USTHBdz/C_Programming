b542f184e39ca2777ce1df6be5f0911c9b112653
Anunnaki <92318629+AliTigui@users.noreply.github.com>
2025-01-27 00:45:32 +0100

    Update array_funcs.h

--- Lecture4/Solutions/array_funcs.h
+++ Lecture4/Solutions/array_funcs.h
@@ -3,29 +3,27 @@
 
 int largest(int size, int *arr){
     int tmp = *arr;
-    for(int i = 0; i< size; i++){
-        if(*(arr+i) > tmp){
+    for(int i = 0; i < size; i++){
+        if(*(arr + i) > tmp){
             tmp = *(arr + i);
-        }
-        
+        }   
     }
     return tmp;
 }
 
 int smallest(int size, int *arr){
     int tmp = *arr;
-    for(int i = 0; i< size; i++){
-        if(*(arr+i) < tmp){
+    for(int i = 0; i < size; i++){
+        if(*(arr + i) < tmp){
             tmp = *(arr + i);
         }
-        
     }
     return tmp;
 }
 
 float avg(int size, int *arr){
     float tmp = 0;
-    for(int i = 0; i< size; i++){
+    for(int i = 0; i < size; i++){
         tmp += (float) *(arr + i) / (float) size;
     }
     return tmp;
